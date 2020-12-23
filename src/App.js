8383ceb94a86c1db0bd873b6e9a932242d9707cd
aarpitgoel <64373216+aarpitgoel@users.noreply.github.com>
2020-12-23 17:08:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./styles.css";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import Dashboard from "./components/Dashboard";
+
+export default function App() {
+  return (
+    <>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/signup" component={Signup} />
+        <Redirect to="/login" />
+      </Switch>
+    </>
+  );
+}
